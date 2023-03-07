e6a98a64137d92c6d6d2948926a058de98e45905
shreya37 <93190521+shreya37@users.noreply.github.com>
2023-03-07 14:30:42 +0530

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $(document).ready(function(){
 <div class="banner">
   <div class="container">
     <div class="banner_info">
-      <h3>Millions of verified Members</h3>
+      <h3>Members of INDIA</h3>
       <a href="register.php" class="hvr-shutter-out-horizontal">Create your Profile</a>
     </div>
   </div>
